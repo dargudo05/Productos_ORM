153db7941a9618e59d7dbc5e3017271d1be3b042
Daniel Argudo <daniel.argudo@sasf.net>
2026-01-27 18:10:39 -0500

    Primer Commit

---
+++ Productos_ORM/Controllers/ProductosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Productos_ORM.Data;
+
+namespace Productos_ORM.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductosController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProductos()
+        {
+            var allProductos = dbContext.Productos.ToList();
+
+            return Ok(allProductos);
+
+        }
+    }
+}
